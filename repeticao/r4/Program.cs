1d69f5755963cb08582d2f21f897e7aa95e5fc16
Gabriela Rosa <gabii.ramos1309@gmail.com>
2023-04-14 10:07:00 -0300

    Exercício 4 concluído

---
+++ repeticao/r4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Faça um programa que leia 10 valores digitados pelo usuário e no final, escreva o maior e o menor valor lido.
+
+int valorMaior = 0;
+int valorMenor = 0;
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"Insira um valor: ");
+    int valor = int.Parse(Console.ReadLine());
+    
+    if (i==1)
+    {
+        valorMaior = valor;
+        valorMenor = valor;
+    }
+    else
+    {
+        if (valor > valorMaior)
+        {
+            valorMaior = valor;
+        }
+        else if (valor < valorMenor)
+        {
+            valorMenor = valor;
+        }
+    }
+}
+
+Console.WriteLine($"O maior valor corresponde a {valorMaior} e o menor valor corresponde a {valorMenor}");
