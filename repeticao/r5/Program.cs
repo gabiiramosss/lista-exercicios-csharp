bbda4f0e6c6503ea3bb1e238cd749a525a196aed
Gabriela Rosa <gabii.ramos1309@gmail.com>
2023-04-17 08:18:59 -0300

    Exercício 5 concluído

---
+++ repeticao/r5/Program.cs
@@ -0,0 +1,12 @@
+﻿// Escreva um algoritmo que imprima a tabuada (de 1 a 10) para os números de 1 a 10.
+// Exemplo: tabuada do 1, tabuada do 2, etc... 
+
+// Dica: utilize um laço dentro do outro.
+
+for (int i = 1; i <= 10; i++)
+{
+    for (int multiplicador = 1; multiplicador <= 10; multiplicador++)
+    {
+        Console.WriteLine($"{multiplicador}x{i} = {multiplicador*i}");
+    }
+}
