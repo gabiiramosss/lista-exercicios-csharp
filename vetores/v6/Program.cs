ec8409cf60c77d2fabaea6b2636ddf6a618f3fe0
Gabriela Rosa <gabii.ramos1309@gmail.com>
2023-04-17 08:46:14 -0300

    Exercício 6 concluído

---
+++ vetores/v6/Program.cs
@@ -0,0 +1,32 @@
+﻿// Escreva um algoritmo que permita a leitura dos nomes de 10 pessoas e armazene os nomes lidos em um vetor. Após isto, o algoritmo deve permitir a leitura de mais 1 nome qualquer de pessoa (para efetuar uma busca) e depois escrever a mensagem ACHEI, se o nome estiver entre os 10 nomes lidos anteriormente (guardados no vetor), ou NÃO ACHEI caso contrário.
+
+string[] nomes = new string [10];
+string nomePesquisa;
+bool encontrou = false;
+
+for (int i = 0; i < nomes.Length; i++)
+{
+    Console.WriteLine($"Insira um nome: ");
+    nomes[i] = Console.ReadLine();
+}
+
+Console.WriteLine($"Pesquise um nome: ");
+nomePesquisa = Console.ReadLine();
+
+for (int i = 0; i < nomes.Length; i++)
+{
+    if (nomePesquisa == nomes[i])
+    {
+        encontrou = true; 
+        break;
+    }
+}
+
+if (encontrou == true)
+{
+    Console.WriteLine($"ACHEI");
+}
+else
+{
+    Console.WriteLine($"NÃO ACHEI");
+}
